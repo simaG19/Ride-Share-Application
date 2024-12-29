4029a2defb7a8863d2621fbc03d0ca4df384bddd
simon <88380975+simaG19@users.noreply.github.com>
2024-12-29 18:10:09 +0300

    password to login_code

--- backend/app/Models/User.php
+++ backend/app/Models/User.php
@@ -17,10 +17,8 @@ class User extends Authenticatable
      *
      * @var array<int, string>
      */
-    protected $fillable = [
-        'name',
-        'email',
-        'password',
+    protected $guarded = [
+
     ];
 
     /**
@@ -29,9 +27,15 @@ class User extends Authenticatable
      * @var array<int, string>
      */
     protected $hidden = [
-        'password',
+        'login_code',
         'remember_token',
     ];
+
+
+    public function routeNotificationForTwilio()
+    {
+        return $this->phone;
+    }
 
     public function driver()
     {
