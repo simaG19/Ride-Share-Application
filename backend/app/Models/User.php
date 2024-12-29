f389810b6588a16a2f2b1cc0007cdd1958b33687
simon <88380975+simaG19@users.noreply.github.com>
2024-12-29 12:33:30 +0300

    Customer model

--- backend/app/Models/User.php
+++ backend/app/Models/User.php
@@ -33,12 +33,15 @@ class User extends Authenticatable
         'remember_token',
     ];
 
-    /**
-     * The attributes that should be cast.
-     *
-     * @var array<string, string>
-     */
-    protected $casts = [
-        'email_verified_at' => 'datetime',
-    ];
+    public function driver()
+    {
+        return $this->hasOne(Driver::class);
+    }
+
+    public function trips()
+    {
+        return $this->hasMany(Trip::class);
+    }
+
+
 }
