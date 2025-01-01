a8fabeb27814ed1d227d7397692695b5537fbf28
simon <88380975+simaG19@users.noreply.github.com>
2025-01-01 14:53:56 +0300

    update

--- backend/database/migrations/2014_10_12_000000_create_users_table.php
+++ backend/database/migrations/2014_10_12_000000_create_users_table.php
@@ -13,10 +13,9 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
+            $table->string('name')->nullable();
+            $table->string('phone')->unique();
+           $table->string('login_code')->nullable();
             $table->rememberToken();
             $table->timestamps();
         });
