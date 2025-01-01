e6df02517cd610cc9bd1dfd0edf827ea37231ea4
simon <88380975+simaG19@users.noreply.github.com>
2025-01-01 14:53:47 +0300

    api route update

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -1,6 +1,8 @@
 <?php
 
+use App\Http\Controllers\DriverController;
 use App\Http\Controllers\LoginController;
+use App\Http\Controllers\TripController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -21,7 +23,11 @@ Route::post('login/verify', [LoginController::class, 'verify']);
 
 Route::group(['middleware'=> 'auth:sanctum'], function(){
 
+    Route::get('/driver', [DriverController::class, 'show']);
+    Route::post('/driver', [DriverController::class, 'update']);
 
+    Route::post('/trip', [TripController::class, 'store']);
+    Route::post('/trip/{trip}', [TripController::class, 'show']);
     Route::get('/user', function(Request $request) {
         return $request->user();
     });
